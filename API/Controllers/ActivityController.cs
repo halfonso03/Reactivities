18b0bcc6453843ac4bfc1c00a80bba8277cc88be
Hector <hector.alfonso@yahoo.com>
2024-11-30 12:35:52 -0500

    end of section 4

--- API/Controllers/ActivityController.cs
+++ API/Controllers/ActivityController.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 
 using System.Linq;
 using System.Threading.Tasks;
+using Application.Activities;
 using Domain;
+using MediatR;
 using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -11,26 +13,44 @@ using Persistence;
 
 namespace API.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")]
     public class ActivitiesController : BaseApiController
     {
-        private readonly DataContext _context;
-        public ActivitiesController(DataContext context)
-        {
-            _context = context;
-        }
-
         [HttpGet]
-        public async Task<ActionResult<List<Activity>>> GetActivities()
+        public async Task<ActionResult<List<Activity>>> GetActivities(CancellationToken ct)
         {
-            return await _context.Activities.ToListAsync();
+            return await Mediator.Send(new List.Query(), ct);
         }
 
         [HttpGet("{id}")]
         public async Task<ActionResult<Activity>> GetActivity(Guid id)
         {
-            return await _context.Activities.FindAsync(id);
+            return await Mediator.Send(new Details.Query { Id = id });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Activity activity)
+        {
+            await Mediator.Send(new Create.Command() { Activity = activity });
+
+            return Ok();
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Edit (Guid id, Activity activity)
+        {
+            activity.Id = id;
+            
+            await Mediator.Send(new Edit.Command { Activity = activity });
+
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete (Guid id)
+        {
+             await Mediator.Send(new Delete.Command { Id = id });
+
+            return Ok();
         }
     }
 }
--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,6 @@
+using API.Extensions;
+using Application.Activities;
+using Application.Core;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
@@ -6,25 +9,9 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddApplicationServices(builder.Configuration);
+
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-builder.Services.AddDbContext<DataContext>(options => 
-{
-    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-
-});
-
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("CorsPolicy",
-        policy =>
-        {
-            policy.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin();
-        });
-});
 
 var app = builder.Build();
 
